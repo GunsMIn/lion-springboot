82f3214c8188e2f528edc583587319e7588acb2a
KimGunWoo9595 <medneter@naver.com>
2022-10-27 13:37:50 +0900

    post api

---
+++ src/main/java/com/springboot/lionboot/controller/PostController.java
@@ -0,0 +1,23 @@
+package com.springboot.lionboot.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+
+    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+    public String postExample() {
+        return "HELLO I.M POST";
+    }
+
+    @PostMapping(value = "/members")
+    public String postMember(@RequestBody Map<String,Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        
+    }
+}
