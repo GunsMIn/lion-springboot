afa0cc576e0352943d3d24d8cda0d6edbc689725
KimGunWoo9595 <medneter@naver.com>
2022-10-28 14:22:38 +0900

    Test beforeach적용

--- src/test/java/com/springboot/lionboot/pratice/domain/controller/UserControllerTest.java
+++ src/test/java/com/springboot/lionboot/pratice/domain/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.springboot.lionboot.pratice.domain.controller;
 import com.springboot.lionboot.pratice.domain.User;
 import com.springboot.lionboot.pratice.domain.repository.UserDao;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,18 @@ class UserControllerTest {
 
     @Autowired private UserDao userDao;
 
+    User user1;
+    User user2;
+    User user3;
+
+    @BeforeEach
+    void setUp() {
+        this.user1 = new User("30", "김xx", "1234");
+        this.user2 = new User("31", "이xx", "1234");
+        this.user3 = new User("32", "박xx", "1234");
+    }
+
+
 
     @Test
     public void 회원가입테스트() {
@@ -27,10 +40,20 @@ class UserControllerTest {
         Assertions.assertEquals(byId.getName(),user.getName());
     }
 
+
+    @Test
+    public void 회원id로삭제() {
+        userDao.deleteById("30")
+        ;
+    }
+
+
     @Test
     public void 회원전체삭제테스트() {
         int value = userDao.deleteAll();
         Assertions.assertEquals(0,value);
     }
+
+
 
 }
