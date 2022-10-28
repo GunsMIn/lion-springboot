2f5f83faa1e07ee2e662c37692a5bd96a509dd19
KimGunWoo9595 <medneter@naver.com>
2022-10-28 13:28:31 +0900

    레이어 계층 실습 controller

--- src/main/java/com/springboot/lionboot/pratice/domain/controller/UserController.java
+++ src/main/java/com/springboot/lionboot/pratice/domain/controller/UserController.java
@@ -1,14 +1,36 @@
 package com.springboot.lionboot.pratice.domain.controller;
 
+import com.springboot.lionboot.pratice.domain.User;
 import com.springboot.lionboot.pratice.domain.repository.UserDao;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
+@RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserDao userDao;
+
+    @PostMapping("/members")
+    public User join(@ModelAttribute User user) {
+        userDao.add(user);
+        log.info("회원가입할 user = {}" , user);
+        return user;
+    }
+
+    @DeleteMapping("/members")
+    public String remove() {
+        userDao.deleteAll();
+        return "유저 전체가 삭제되었습니다";
+    }
+
+
 
 
 
