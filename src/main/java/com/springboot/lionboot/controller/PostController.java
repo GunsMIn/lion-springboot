ec4da34cbdf8997953050af9fa77df8db61b1dd9
KimGunWoo9595 <medneter@naver.com>
2022-10-28 11:31:12 +0900

    @Slf4J 로그 적용 (postMapping)

--- src/main/java/com/springboot/lionboot/controller/PostController.java
+++ src/main/java/com/springboot/lionboot/controller/PostController.java
@@ -2,12 +2,14 @@ package com.springboot.lionboot.controller;
 
 
 import com.springboot.lionboot.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
+@Slf4j
 public class PostController {
 
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
@@ -31,6 +33,7 @@ public class PostController {
 
     @PostMapping(value = "/members3")
     public MemberDto postdto2(@RequestBody MemberDto memberDto) {
+        log.info("다음과 같은 member가 들어왔습니다 member :{}",memberDto);
         return memberDto;
     }
 }
