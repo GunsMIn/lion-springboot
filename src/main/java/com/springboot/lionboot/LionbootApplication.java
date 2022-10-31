23781403243e0a7cb473e470c33e480b7ecf4346
KimGunWoo9595 <medneter@naver.com>
2022-10-31 14:24:03 +0900

    application

---
+++ src/main/java/com/springboot/lionboot/LionbootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.lionboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LionbootApplication.class, args);
+    }
+
+}
