e0efeeda4aafa724a4c5a1764ce95f068d60202d
KimGunWoo9595 <medneter@naver.com>
2022-11-01 14:19:13 +0900

    insert / tdd

--- src/main/java/com/springboot/lionboot/pratice/repository/HospitalDao.java
+++ src/main/java/com/springboot/lionboot/pratice/repository/HospitalDao.java
@@ -16,19 +16,21 @@ public class HospitalDao {
 
     //list<Hospital> -- 11만건 hospital
     public void add(Hospital hospital) {
-        String sql = "INSERT INTO nationwide_hospitals(id, open_service_name, open_local_government_code, management_number, license_date, business_status, business_status_code, phone, full_address, road_name_address, hospital_name, business_type_name, healthcare_provider_count, patient_room_count, total_number_of_beds, total_area_size)" +
-                " VALUES(?, ?, ?," +
-                " ?, ?, ?," +
-                " ?, ?, ?," +
-                " ?, ?, ?," +
-                " ?, ?, ?," +
-                " ?);";
-        this.jdbcTemplate.update(sql, hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
+        String sql = "INSERT INTO `likelion-db`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`)" +
+                " VALUES (?,?,?," +
+                " ?,?,?," +
+                " ?,?,?," +
+                " ?,?,?," +
+                " ?,?,?," +
+                " ?);"; // 16개
+        this.jdbcTemplate.update(sql,
+                hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
                 hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(),
                 hospital.getBusinessStatusCode(), hospital.getPhone(), hospital.getFullAddress(),
                 hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(),
                 hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(),
-                hospital.getTotalAreaSize());
+                hospital.getTotalAreaSize()
+        );
     }
 
-}
+    }
--- src/test/java/com/springboot/lionboot/pratice/readlineparser/HospitalParserTest.java
+++ src/test/java/com/springboot/lionboot/pratice/readlineparser/HospitalParserTest.java
@@ -1,6 +1,7 @@
 package com.springboot.lionboot.pratice.readlineparser;
 
 import com.springboot.lionboot.pratice.domain.Hospital;
+import com.springboot.lionboot.pratice.repository.HospitalDao;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +15,31 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//@SpringBootTest
+//@SpringBootApplication —> 얘가 스캔해서 등록한 Bean들을 가져다 씁니다.
 @SpringBootTest // 스프링부트 환경에서 테스트 할 수 있게 해주는 어노테이션
 class HospitalParserTest {
 
     String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
 
-
     @Autowired
     ReadLineContext<Hospital> hospitalReadLineContext;
+
+    @Autowired
+    HospitalDao hospitalDao;
+
+
+
+    @Test
+    @DisplayName("sql insert tdd")
+    void insert() {
+        HospitalParser hp = new HospitalParser();
+        Hospital hospital = hp.parse(line1);
+        hospitalDao.add(hospital);
+        //
+    }
+
+
 
     @Test
     @DisplayName("10만건 이상 데이터가 파싱 되는지")
@@ -41,8 +58,6 @@ class HospitalParserTest {
     @Test
     @DisplayName("csv 1줄은 Hospital로 잘 만드는지 TEST")
     void convertToHospital(){
-
-
         HospitalParser hp = new HospitalParser();
         Hospital hospital = hp.parse(line1);
 
@@ -63,5 +78,8 @@ class HospitalParserTest {
         assertEquals(0, hospital.getTotalNumberOfBeds());
         assertEquals(52.29f, hospital.getTotalAreaSize());
     }
+
+
+
 
 }
