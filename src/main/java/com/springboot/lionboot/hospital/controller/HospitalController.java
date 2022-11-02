2dc6049fa9faaf6902fca8492e335c21ad1f9ef6
KimGunWoo9595 <medneter@naver.com>
2022-11-02 11:34:21 +0900

    REST컨트롤러(findById,count,insert)

--- src/main/java/com/springboot/lionboot/hospital/controller/HospitalController.java
+++ src/main/java/com/springboot/lionboot/hospital/controller/HospitalController.java
@@ -3,10 +3,10 @@ package com.springboot.lionboot.hospital.controller;
 import com.springboot.lionboot.hospital.dao.HospitalDao;
 import com.springboot.lionboot.hospital.domain.Hospital;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,9 +14,21 @@ public class HospitalController {
 
     private final HospitalDao hospitalDao;
 
-    @GetMapping("/members/{id}")
+    @GetMapping("/hospitals/{id}")
     public Hospital findById(@PathVariable int id) {
         Hospital hospital = hospitalDao.findById(id);
+        return hospital;
+    }
+
+    @GetMapping("/hospitals")
+    public int getCount() {
+        int count = hospitalDao.getCount();
+        return count;
+    }
+
+    @PostMapping("/hospitals/{id}")
+    public Hospital addHospital(@ModelAttribute Hospital hospital) {
+        hospitalDao.add(hospital);
         return hospital;
     }
 
